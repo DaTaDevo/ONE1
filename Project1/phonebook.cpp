caa524b439ac7081ef3980941b9bceca421d3b12
DaTaDevo <40675303+DaTaDevo@users.noreply.github.com>
2019-01-10 20:12:37 +0300

    PhoneBook v.0.5 (beta v0.9)
    +add a vector for transefer data to console
    +set up some function under new den

--- Project1/phonebook.cpp
+++ Project1/phonebook.cpp
@@ -1,11 +1,44 @@
 #include "phonebook.h"
 
+std::string beforeInput(Contact user)
+{
+    int a=0;
 
-void SaveInputData(Contact user)
+    std::cout << "1.Open file 2.New file "<<std::endl;
+    std::cin >> a;
+    if(a == 2)
+    {
+        std::cout << "Type a name of file, which you wanna create" <<std::endl;
+        std::cin >> user.nameOfFile;
+
+        std::fstream newFile;
+        newFile.open(user.nameOfFile + ".txt");
+        newFile.close();
+    }
+    else
+    {
+        std::cout << "Type a name of file, which you wanna open: ";
+        std::cin >> user.nameOfFile;
+        std::cout<<std::endl;
+
+        std::fstream tempFile;
+        tempFile.open(user.nameOfFile + ".txt",std::ios::out);
+
+        if(tempFile.is_open() == false)
+        {return user.nameOfFile;
+            std::cout<<"Error:File not found!"<<std::endl;
+            beforeInput(user);
+        }
+        tempFile.close();
+    }
+  return user.nameOfFile;
+}
+
+void saveInputData(Contact user)
 {
 
     std::ofstream fileSave;
-    fileSave.open("PhoneBook.txt", std::ifstream::app);
+    fileSave.open(user.nameOfFile, std::ifstream::app);
 
     if (fileSave.is_open())
     {
@@ -19,43 +52,55 @@ void SaveInputData(Contact user)
     {
        std:: cout << "Error: File open feiled" << std::endl;
     }
-    fileSave.close();
 }
 
-void InputData(Contact user)
+void inputData(Contact user)
 {
-    std:: cout << "Name - "; std::cin >> user.name;
-    std:: cout << "Number - "; std::cin >> user.number;std::cout << std::endl;
+ //создание или открытия опеределенного файла-контактов пользователем(DONE!)
+ int a = 0;
+
+ beforeInput(user);
+ std::cout << "Name - ";
+ std::cin  >> user.name;
+
+ std::cout << "Number - ";
+ std::cin  >> user.number;
+ std::cout << std::endl;
 
 
-    std:: cout <<  "Total:"   << std::endl;
-    std:: cout << "Name - "   << user.name << std::endl
-                    << "Number - " << user.number << std::endl << std::endl;
+ std::cout <<  "Total:"   << std::endl;
+ std::cout << "Name - "   << user.name << std::endl
+           << "Number - " << user.number << std::endl << std::endl;
 
-    std::cout << "Save ?" << std::endl << "1. Yes   2.No" << std::endl ;
+ std::cout << "Save ?" << std::endl << "1. Yes   2.No" << std::endl ;
 
-    int a =0;
-    std::cin >> a;
+ std::cin  >> a;
 
-    if (a == 1)
-    {
-        SaveInputData(user);
-    }
-    else
-    {
-        InputData(user);
-    }
+ if (a == 1)
+ {
+     saveInputData(user);
+ }
+ else
+ {
+     inputData(user);
+ }
 }
 
-void OutputPhoneBook()
+void outputPhoneBook(Contact user)
 {
+    //вывод на экран по 10 контактов(для вектора 10 контактов = 40 элементов)(WAIT...)
+    std::cout<<"Type a name of file: ";
+    std::cin >>user.nameOfFile;
+    std::cout<<std::endl;
+
     std::string strOutput;
     std::ifstream fileOut;
-    fileOut.open("PhoneBook.txt");
+
+    fileOut.open(user.nameOfFile + ".txt");
     while (!fileOut.eof())
     {
         fileOut >> strOutput;
-        std::cout << strOutput<<std::endl;
+        user.transfer.push_back(strOutput);
         strOutput = " ";
     }
     fileOut.close();
@@ -70,10 +115,10 @@ void menu(Contact user)
 
     if (a == 1)
     {
-        InputData(user);
+        inputData(user);
     }
     else
     {
-        OutputPhoneBook();
+        outputPhoneBook();
     }
 }
--- Project1/phonebook.h
+++ Project1/phonebook.h
@@ -2,17 +2,21 @@
 #define PHONEBOOK_H
 #include <string>
 #include <iostream>
-# include <fstream>
+#include <fstream>
+#include <vector>
 
  class  Contact
  {
  public :
      std::string name;
      std::string number;
+     std::string nameOfFile;
+     std::vector <std::string> transfer;
  };
-void SaveInputData(Contact user);
+std::string beforeInput(Contact user);
+void saveInputData(Contact user);
 void menu(Contact user);
-void InputData(Contact user);
-void OutputPhoneBook();
+void inputData(Contact user);
+void outputPhoneBook();
 
 #endif // PHONEBOOK_H
